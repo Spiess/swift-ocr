6cf4ba5adf2c68a887c3a728435b459941241073
Florian Spiess <florian.spiess@unibas.ch>
2024-12-17 18:19:08 +0100

    Update for two level directory and writing into a single file

--- swift-ocr/main.swift
+++ swift-ocr/main.swift
@@ -20,14 +20,14 @@ struct SwiftOCR: ParsableCommand {
     @Argument(help:"The path to a directory of image files to extract text from")
     var inputDirectory: String
     
-    @Argument(help:"The path to a directory where output JSON files should be saved")
-    var outputDirectory: String
+    @Argument(help:"The path to the JSONL file to save the extracted text")
+    var outputFile: String
     
     func run() throws {
         let logger = Logger(label: "TextExtractCLI")
         
         logger.info("Input directory: \(inputDirectory)")
-        logger.info("Output directory: \(outputDirectory)")
+        logger.info("Output file: \(outputFile)")
         
         let fm = FileManager.default
         
@@ -39,64 +39,75 @@ struct SwiftOCR: ParsableCommand {
             throw(Errors.notDirectory)
         }
         
-        if (!fm.fileExists(atPath: outputDirectory, isDirectory: &isDir)) {
-            logger.info("Creating output directory")
-            try fm.createDirectory(atPath: outputDirectory, withIntermediateDirectories: true)
-        } else if (isDir.boolValue) {
-            logger.info("Output directory already exists")
+        let subdirs = try fm.contentsOfDirectory(atPath: inputDirectory)
+        
+        try "".write(to: URL(fileURLWithPath: outputFile), atomically: true, encoding: .utf8)
+        
+        if let fileHandle = FileHandle(forWritingAtPath: outputFile) {
+            defer {
+                fileHandle.closeFile()
+            } // Ensure the file is closed afterward
+            for subdir in subdirs {
+                let subdirPath = NSString.path(withComponents: [inputDirectory, subdir])
+                logger.info("Working in \"\(subdirPath)\"")
+                
+                let files = try fm.contentsOfDirectory(atPath: subdirPath)
+                
+                let images = files.filter { $0.hasSuffix(".jpg") || $0.hasSuffix(".png") }
+                
+                for imPath in images {
+                    var detectionsDict: [String: [[String: Any]]] = [:]
+                    
+                    let path = NSString.path(withComponents: [subdirPath, imPath])
+                    logger.info("Processing \"\(path)\"")
+                    guard let im = NSImage(byReferencingFile:path) else {
+                        throw(Errors.invalidImage)
+                    }
+                    
+                    guard let cgImage = im.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
+                        throw(Errors.cgImage)
+                    }
+                    
+                    let rsp = ProcessImage(image: cgImage)
+                    
+                    switch rsp {
+                    case .failure(let error):
+                        throw(error)
+                    case .success(let detections):
+                        let width = Float(cgImage.width)
+                        let height = Float(cgImage.height)
+                        // If there are no detections, continue
+                        if detections.isEmpty { continue }
+                        detectionsDict["detections"] = detections.map({ (text, rect, confidence) in
+                            let x = Float(rect.minX)
+                            let y = Float(rect.minY)
+                            let w = Float(rect.maxX - rect.minX)
+                            let h = Float(rect.maxY - rect.minY)
+                            return [
+                                "image": imPath,
+                                "text": text,
+                                "x": x,
+                                "y": y,
+                                "w": w,
+                                "h": h,
+                                "relX": x / width,
+                                "relY": y / height,
+                                "relW": w / width,
+                                "relH": h / height,
+                                "confidence": confidence
+                            ]
+                        })
+                        let jsonData = try JSONSerialization.data(withJSONObject: detectionsDict)
+                        let jsonString = String(bytes: jsonData, encoding: .utf8)!
+                        fileHandle.write((jsonString + "\n").data(using: .utf8)!)
+                    }
+                }
+            }
         } else {
-            throw(Errors.couldNotCreateOutput)
+            print("Error: Could not open file for writing.")
         }
         
-        let files = try fm.contentsOfDirectory(atPath: inputDirectory)
         
-        let images = files.filter { $0.hasSuffix(".jpg") || $0.hasSuffix(".png") }
-        
-        for imPath in images {
-            var detectionsDict: [String: [[String: Any]]] = [:]
-            
-            let path = NSString.path(withComponents: [inputDirectory, imPath])
-            logger.info("Processing \"\(path)\"")
-            guard let im = NSImage(byReferencingFile:path) else {
-                throw(Errors.invalidImage)
-            }
-            
-            guard let cgImage = im.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
-                throw(Errors.cgImage)
-            }
-            
-            let rsp = ProcessImage(image: cgImage)
-            
-            switch rsp {
-            case .failure(let error):
-                throw(error)
-            case .success(let detections):
-                let width = Float(cgImage.width)
-                let height = Float(cgImage.height)
-                detectionsDict["detections"] = detections.map({ (text, rect, confidence) in
-                    let x = Float(rect.minX)
-                    let y = Float(rect.minY)
-                    let w = Float(rect.maxX - rect.minX)
-                    let h = Float(rect.maxY - rect.minY)
-                    return [
-                        "text": text,
-                        "x": x,
-                        "y": y,
-                        "w": w,
-                        "h": h,
-                        "relX": x / width,
-                        "relY": y / height,
-                        "relW": w / width,
-                        "relH": h / height,
-                        "confidence": confidence
-                    ]
-                })
-                let jsonData = try JSONSerialization.data(withJSONObject: detectionsDict, options: .prettyPrinted)
-                let jsonString = String(bytes: jsonData, encoding: .utf8)!
-                let outPath = NSString.path(withComponents: [outputDirectory, imPath + ".txt"])
-                try jsonString.write(toFile: outPath, atomically: true, encoding: .utf8)
-            }
-        }
     }
     
     func ProcessImage(image: CGImage) -> Result<[(String, CGRect, VNConfidence)], Error> {
